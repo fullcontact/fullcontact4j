d06bd8582d43cbce674e90dc35557c7064a85231
Paris Mitton <paris@fullcontact.com>
2014-11-06 11:11:40 -0700

    use better rate limiting logic

--- src/main/java/com/fullcontact/api/libs/fullcontact4j/http/FCRetrofitCallback.java
+++ src/main/java/com/fullcontact/api/libs/fullcontact4j/http/FCRetrofitCallback.java
@@ -15,6 +15,7 @@ import retrofit.converter.ConversionException;
  * Before FCCallback receives the success() or failure() call,
  * this callback acts as an interceptor to update rate limits from headers
  * and provide a useful FullContactException from a RetrofitError.
+ *
  * @param <T>
  */
 public class FCRetrofitCallback<T extends FCResponse> implements Callback<T> {
@@ -28,19 +29,22 @@ public class FCRetrofitCallback<T extends FCResponse> implements Callback<T> {
     }
 
     public void success(T t, Response response) {
-        //intercept headers
-        try {
+        RequestExecutorHandler requestHandler = httpInterface.getRequestExecutorHandler();
+        //should we comb the headers for rate limit info?
+        if (requestHandler.shouldNotfiyRateLimit()) {
             for (Header h : response.getHeaders()) {
                 if (FCConstants.HEADER_RATE_LIMIT_PER_MINUTE.equals(h.getName())) {
                     Utils.verbose("Updated rate limit based on response headers from FullContact.");
-                    httpInterface.getRequestExecutorHandler().setRateLimitPerMinute(Integer.parseInt(h.getValue()));
+                    try {
+                        requestHandler.notifyRateLimitPerMinute(Integer.parseInt(h.getValue()));
+                    } catch (NumberFormatException e) {
+                        //rate limit per minute wasn't a number (???), don't set any limits
+                        e.printStackTrace();
+                        Utils.info("FullContact response had a rate limit that was not a number.");
+                    }
                     break;
                 }
             }
-        } catch(NumberFormatException e) {
-            //rate limit per minute wasn't a number (???), don't set any limits
-            e.printStackTrace();
-            Utils.info("FullContact response had a rate limit that was not a number.");
         }
 
         parent.success(t);
@@ -52,7 +56,7 @@ public class FCRetrofitCallback<T extends FCResponse> implements Callback<T> {
         //do some logic to figure out why this error occurred
         String reason = "Unknown reason for exception, see stack trace";
         Response response = retrofitError.getResponse();
-        switch(retrofitError.getKind()) {
+        switch (retrofitError.getKind()) {
             case CONVERSION:
                 reason = "Encountered an error converting to a Java object from response JSON";
                 break;
@@ -66,13 +70,13 @@ public class FCRetrofitCallback<T extends FCResponse> implements Callback<T> {
                     ErrorResponse errorResponse = (ErrorResponse) httpInterface.getJsonConverter()
                             .fromBody(response.getBody(),
                                     ErrorResponse.class);
-                    reason = (errorResponse.message==null?"":errorResponse.message);
-                } catch(ConversionException e) {
+                    reason = (errorResponse.message == null ? "" : errorResponse.message);
+                } catch (ConversionException e) {
                     //response did not have a formatted error response...should not happen
                     ex = e;
                     reason = "Although the FullContact API responded with an error, " +
                             "the response was not in the proper format.";
-                } catch(ClassCastException e) {
+                } catch (ClassCastException e) {
                     ex = e;
                     reason = "Although the FullContact API responded with an error, " +
                             "the response was not in the proper format.";
@@ -81,6 +85,6 @@ public class FCRetrofitCallback<T extends FCResponse> implements Callback<T> {
             default:
                 break;
         }
-        parent.failure(new FullContactException(reason, (response==null?null:response.getStatus()), ex));
+        parent.failure(new FullContactException(reason, (response == null ? null : response.getStatus()), ex));
     }
 }
--- src/main/java/com/fullcontact/api/libs/fullcontact4j/http/RequestExecutorHandler.java
+++ src/main/java/com/fullcontact/api/libs/fullcontact4j/http/RequestExecutorHandler.java
@@ -7,6 +7,7 @@ import com.fullcontact.api.libs.fullcontact4j.guava.RateLimiter;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This class handles requests made by the client.
@@ -15,34 +16,49 @@ import java.util.concurrent.Executors;
  */
 public class RequestExecutorHandler {
 
+    //the default reqs per second. when rate limit headers come back from the client, those will be used instead.
+    private static final int DEFAULT_REQUESTS_PER_SECOND = 10;
+    // how often to check for a rate limit change
+    private static final long RATE_LIMIT_CHECK_INTERVAL_MS = TimeUnit.MINUTES.convert(5, TimeUnit.MILLISECONDS);
+
     //will execute the requests on a separate thread.
     protected final ExecutorService executorService;
 
     //if not null, will limit the request rate.
     private RateLimiter rateLimiter;
+    private volatile long lastRateLimitCheck = 0;
     private final RateLimiterPolicy policy;
 
     public RequestExecutorHandler(RateLimiterPolicy policy, Integer threadPoolCount) {
         this.policy = policy;
         executorService = Executors.newFixedThreadPool(threadPoolCount);
+        if(policy == RateLimiterPolicy.BURST) {
+            rateLimiter = RateLimiter.create(RateLimiter.SleepingStopwatch.createFromSystemTimer(),
+                    DEFAULT_REQUESTS_PER_SECOND, 5.0);
+        } else {
+            rateLimiter = RateLimiter.create(DEFAULT_REQUESTS_PER_SECOND);
+        }
     }
 
     /**
-     * Sets the maximum requests per second. Requests are unlimited until the first response,
-     * when the maximum amount is set from the rate limiting headers. It cannot be changed after that.
+     * If the check interval time has passed, update the rate limit
      * @param requestsPerMinute
      */
-    public void setRateLimitPerMinute(Integer requestsPerMinute) {
-        if(rateLimiter == null) {
+    public synchronized void notifyRateLimitPerMinute(Integer requestsPerMinute) {
+        // this check will double-check if updating the rate limit is valid, but
+        // ideally clients should call shouldNotifyRateLimit() before wasting time blocking on this method
+        if(shouldNotfiyRateLimit()) {
             Integer requestsPerSecond = requestsPerMinute / 60;
-
-            if(policy == RateLimiterPolicy.BURST) {
-                rateLimiter = RateLimiter.create(RateLimiter.SleepingStopwatch.createFromSystemTimer(),
-                        requestsPerSecond, 5.0);
-            } else {
-                rateLimiter = RateLimiter.create(requestsPerSecond);
-            }
+            rateLimiter.setRate(requestsPerSecond);
+            lastRateLimitCheck = System.currentTimeMillis();
         }
+    }
+
+    /**
+     * Has RATE_LIMIT_CHECK time passed since we last made a rate limit update?
+     */
+    public boolean shouldNotfiyRateLimit() {
+        return System.currentTimeMillis() - lastRateLimitCheck > RATE_LIMIT_CHECK_INTERVAL_MS;
     }
 
     public <T extends FCResponse> void sendRequestAsync(final FullContactApi api, final FCRequest<T> req,
